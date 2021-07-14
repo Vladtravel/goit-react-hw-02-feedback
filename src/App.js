8e9c3a16c7918dbfcf929a65f789b16315cf8997
Vladyslav <vladtravel2015@gmail.com>
2021-07-14 22:21:33 +0300

    финиш

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Section from "./components/Section";
+import Statistics from "./components/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Notification from "./components/Notification";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onBtnCLick = (option) => () => {
+    this.setState((prevState) => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => this.state.good + this.state.neutral + this.state.bad;
+  countPositiveFeedbackPercentage = () =>
+    Math.round((this.state.good / (this.state.good + this.state.neutral + this.state.bad)) * 100);
+
+  render() {
+    const option = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+        <Section title={"Please leave feedback"}>
+          <FeedbackOptions options={option} onLeaveFeedback={this.onBtnCLick} />
+        </Section>
+
+        <Section title={"Statistics"}>
+          {total === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </>
+    );
+  }
 }
 
 export default App;
